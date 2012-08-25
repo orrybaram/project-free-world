d2f14ae6caee32b63c1e6725148a763defc8f8a9
jamesjn <james.jn.chiang@gmail.com>
2012-08-25 16:54:20 -0400

    auto push

--- static/app/geomap.js
+++ static/app/geomap.js
@@ -1,6 +1,6 @@
 
 $(document).ready(){
-  mapOptions = {}
+  mapOptions = {zoom: 5, mapTypeId: googlemaps.MapTypeId.TERRAIN}
   var map = new google.maps.Map(document.getElementById("map_canvas")), mapOptions) 
 }
 
