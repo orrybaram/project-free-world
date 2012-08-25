ee116a813a07c1dbef0c3d84866cc346a4aa7653
jamesjn <james.jn.chiang@gmail.com>
2012-08-25 17:03:38 -0400

    auto push

--- static/app/geomap.js
+++ static/app/geomap.js
@@ -1,9 +1,9 @@
 
-$(document).ready(){
+$(document).ready(function(){
   mapOptions = {zoom: 5, center: new google.maps.LatLng(-34.397, 150.644), mapTypeId: googlemaps.MapTypeId.TERRAIN}
-  var map = new google.maps.Map(document.getElementById("map_canvas")), mapOptions) 
-}
+  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions) 
+});
 
 if(typeof GeoMap === undefined || GeoMap === null){
   GeoMap = {};
-}
+};
