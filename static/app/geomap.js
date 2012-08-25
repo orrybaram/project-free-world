f5b79ffac373ee54c0da4dabf979539703939453
jamesjn <james.jn.chiang@gmail.com>
2012-08-25 17:13:38 -0400

    auto push

--- static/app/geomap.js
+++ static/app/geomap.js
@@ -1,7 +1,6 @@
-
 $(document).ready(function(){
-  mapOptions = {zoom: 5, center: new google.maps.LatLng(-34.397, 150.644), mapTypeId: googlemaps.MapTypeId.TERRAIN}
-  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions) 
+  mapOptions = {zoom: 5, center: new google.maps.LatLng(-34.397, 150.644), mapTypeId: googlemaps.MapTypeId.TERRAIN};
+  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 });
 
 if(typeof GeoMap === undefined || GeoMap === null){
