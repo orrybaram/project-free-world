11e28dfbdf0daed9228625aad647c6adc7546069
jamesjn <james.jn.chiang@gmail.com>
2012-08-26 14:12:19 -0400

    add trouble spots json

---
+++ static/app/trouble_spots.js
@@ -0,0 +1,308 @@
+TS = {};
+
+TS.poverty = [
+	{
+		"Countries by Continent": "Somalia",
+		"LatLng":[10,49], 
+		"Type": "Poverty Alleviation", 
+		"Value": "3",
+		"Lat": "02°02'N",
+		"Lng": "45°25'E"
+	},
+	{
+		"Countries by Continent": "Haiti",
+		"LatLng": [19,-72.4167], 
+		"Type": "Poverty Alleviation", 
+		"Value": "5",
+		"Lat": "18°40'N",
+		"Lng": "72°20'W"
+	},
+	{
+		"Countries by Continent": "Guinea-Bissau",
+		"LatLng": [12,-15], 
+		"Type":	"Poverty Alleviation",
+		 "Value": "10",
+		"Lat": "11°45'N",
+		"Lng": "15°45'W"
+	},
+	{
+		"Countries by Continent": "Guinea",
+		"LatLng": [11,-10], 
+		"Type": 		"Poverty Alleviation",
+		 "Value": "11",
+		"Lat": "09°29'N",
+		"Lng": "13°49'W"
+	},
+	{
+		"Countries by Continent": "Zimbabwe",
+		"LatLng": [-20,30],
+		 "Type": "Poverty Alleviation", 
+		"Value": "11",
+		"Lat": "17°43'S",
+		"Lng": "31°02'E"
+	},
+	{
+		"Countries by Continent": "Burundi",
+		"LatLng": [-3.5,30], 
+		"Type": "Poverty Alleviation",
+		 "Value": "12",
+		"Lat": "03°16'S",
+		"Lng": "29°18'E"
+	},
+];
+
+TS.economic= [
+	{
+		"Countries by Continent": "Angola",
+		"LatLng": [-12.5,18.5], 
+		"Type":		"Economic Equality",
+		 "Value": "9",
+		"Lat": "08°50'S",
+		"Lng": "13°15'E"
+	},
+	{
+		"Countries by Continent": "D R Congo",
+		"LatLng": [0,25], 
+		"Type":		"Economic Equality",
+		 "Value": "11",
+		"Lat": "39°09'N",
+		"Lng": "125°30'E"
+	},
+	{
+		"Countries by Continent": "Nigeria",
+		"LatLng": [10,8], 
+		"Type":		"Economic Equality", 
+		"Value": "11",
+		"Lat": "09°05'N",
+		"Lng": "07°32'E"
+	},
+	{
+		"Countries by Continent": "Zimbabwe",
+		"LatLng": [-20,30], 
+		"Type":		"Economic Equality", 
+		"Value": "11",
+		"Lat": "17°43'S",
+		"Lng": "31°02'E"
+	},
+	{
+		"Countries by Continent": "Equatorial Guinea",
+		"LatLng": [2,10], 
+		"Type":		"Economic Equality", 
+		"Value": "12",
+		"Lat": "03°45'N",
+		"Lng": "08°50'E"
+	},
+	{
+		"Countries by Continent": "South Sudan",
+		"LatLng": [5.88, 31.28], 
+		"Type":	"Economic Equality", 
+		"Value": "12",
+		"Lat": "#N/A",
+		"Lng": "#N/A"
+	}
+]
+
+TS.infrastructure= [
+	{
+		"Countries by Continent": "Somalia",
+		"LatLng": [10,49], 
+		"Type": 		"Infrastructure Index", 
+		"Value": "4",
+		"Lat": "02°02'N",
+		"Lng": "45°25'E"
+	},
+	{
+		"Countries by Continent": "D R Congo",
+		"LatLng": [0,25], 
+		"Type": 		"Infrastructure Index", 
+		"Value": "9",
+		"Lat": "39°09'N",
+		"Lng": "125°30'E"
+	},
+	{
+		"Countries by Continent": "Chad",
+		"LatLng": [15,19],
+		 "Type": 		"Infrastructure Index", 
+		 "Value": "10",
+		"Lat": "12°10'N",
+		"Lng": "14°59'E"
+	},
+	{
+		"Countries by Continent": "Zimbabwe",
+		"LatLng": [-20,30], 
+		"Type":		"Infrastructure Index", 
+		"Value": "11",
+		"Lat": "17°43'S",
+		"Lng": "31°02'E"
+	},
+	{
+		"Countries by Continent": "Haiti",
+		"LatLng": [19,-72.4167], 
+		"Type":		"Infrastructure Index", 
+		"Value": "11",
+		"Lat": "18°40'N",
+		"Lng": "72°20'W"
+	},
+	{
+		"Countries by Continent": "South Sudan",
+		"LatLng":  [5.88, 31.28],
+		 "Type":		"Infrastructure Index", 
+		"Value": "14",
+		"Lat": "#N/A",
+		"Lng": "#N/A"
+	}
+]
+
+TS.human = [
+	{
+		"Countries by Continent": "Somalia",
+		"LatLng":[10,49], 
+		"Type": 	"Human Rights Index", 
+		"Value": "2",
+		"Lat": "02°02'N",
+		"Lng": "45°25'E"
+	},
+	{
+		"Countries by Continent": "South Sudan",
+		"LatLng": [5.88, 31.28], 
+		"Type":		"Human Rights Index", 
+		"Value": "4",
+		"Lat": "#N/A",
+		"Lng": "#N/A"
+	},
+	{
+		"Countries by Continent": "D R Congo",
+		"LatLng": [0,25],
+		 "Type":	"Human Rights Index", 
+		 "Value": "5",
+		"Lat": "39°09'N",
+		"Lng": "125°30'E"
+	},
+	{
+		"Countries by Continent": "Sudan",
+		"LatLng": [15,30],
+		 "Type": 		"Human Rights Index", 
+		 "Value": "6",
+		"Lat": "15°31'N",
+		"Lng": "32°35'E"
+	},
+	{
+		"Countries by Continent": "Syria",
+		"LatLng": [35,38], 
+		"Type": 		"Human Rights Index", 
+		"Value": "7",
+		"Lat": "#N/A",
+		"Lng": "#N/A"
+	},
+	{
+		"Countries by Continent": "Chad",
+		"LatLng": [15,19], 
+		"Type": 		"Human Rights Index", 
+		"Value": "8",
+		"Lat": "12°10'N",
+		"Lng": "14°59'E"
+	}
+]
+
+TS.government = [
+	{
+		"Countries by Continent": "Somalia",
+		"LatLng": [10,49], 
+		"Type": 		"Government Legitimacy", 
+		"Value": "1",
+		"Lat": "02°02'N",
+		"Lng": "45°25'E"
+	},
+	{
+		"Countries by Continent": "South Sudan",
+		"LatLng": [5.88, 31.28], 
+		"Type": 		"Government Legitimacy", 
+		"Value": "3",
+		"Lat": "#N/A",
+		"Lng": "#N/A"
+	},
+	{
+		"Countries by Continent": "Afghanistan",
+		"LatLng": [33,65], 
+		"Type": 		"Government Legitimacy", 
+		"Value": "3",
+		"Lat": "34°28'N",
+		"Lng": "69°11'E"
+	},
+	{
+		"Countries by Continent": "D R Congo",
+		"LatLng": [0,25], 
+		"Type": 		"Government Legitimacy", 
+		"Value": "4",
+		"Lat": "39°09'N",
+		"Lng": "125°30'E"
+	},
+	{
+		"Countries by Continent": "Ivory Coast",
+		"LatLng": [8,-5], 
+		"Type": 		"Government Legitimacy", 
+		"Value": "4",
+		"Lat": "#N/A",
+		"Lng": "#N/A"
+	},
+	{
+		"Countries by Continent": "Sudan",
+		"LatLng": [15,30], 
+		"Type": 		"Government Legitimacy", 
+		"Value": "4",
+		"Lat": "15°31'N",
+		"Lng": "32°35'E"
+	}
+]
+
+TS.literacy = [
+	{
+		"Countries by Continent": "Mali",
+		"LatLng": [17,-4], 
+		"Type": 	"Literacy Rate", 
+		"Value": "26",
+		"Lat": "12°34'N",
+		"Lng": "07°55'W"
+	},
+	{
+		"Countries by Continent": "South Sudan",
+		"LatLng": [5.88, 31.28], 
+		"Type": 		"Literacy Rate",
+		 "Value": "27",
+		"Lat": "#N/A",
+		"Lng": "#N/A"
+	},
+	{
+		"Countries by Continent": "Sudan",
+		"LatLng": [15,30],
+		"Type": 		"Literacy Rate",
+		"Value": "27",
+		"Lat": "15°31'N",
+		"Lng": "32°35'E"
+	},
+	{
+		"Countries by Continent": "Ethiopia",
+		"LatLng": [8,38], 
+		"Type":  "Literacy Rate", 
+		"Value": "28",
+		"Lat": "09°02'N",
+		"Lng": "38°42'E"
+	},
+	{
+		"Countries by Continent": "Burkina Faso",
+		"LatLng": [13,-2], 
+		"Type":  "Literacy Rate", 
+		"Value": "29",
+		"Lat": "12°15'N",
+		"Lng": "01°30'W"
+	},
+	{
+		"Countries by Continent": "Niger",
+		"LatLng": [16,8],
+		 "Type": 	"Literacy Rate", 
+		"Value": "29",
+		"Lat": "13°27'N",
+		"Lng": "02°06'E"
+	}
+]
+
