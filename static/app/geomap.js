1b0c04f3efff985350a3ab4e95c8a9304b1fdc79
jamesjn <james.jn.chiang@gmail.com>
2012-08-26 12:03:47 -0400

    auto check in

--- static/app/geomap.js
+++ static/app/geomap.js
@@ -128,4 +128,16 @@ $(document).ready(function(){
       GeoMap.map.setZoom(polygon_zoom_level);
     });
   };
+  google.maps.Polygon.prototype.getBounds = function() {
+      var bounds = new google.maps.LatLngBounds();
+      var paths = this.getPaths();
+      var path;        
+      for (var i = 0; i < paths.getLength(); i++) {
+          path = paths.getAt(i);
+          for (var ii = 0; ii < path.getLength(); ii++) {
+              bounds.extend(path.getAt(ii));
+          }
+      }
+      return bounds;
+  }
 })();
