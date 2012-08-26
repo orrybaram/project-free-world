45818d3ccb05d2cbe7f0f57ebd86537c6bc29136
jamesjn <james.jn.chiang@gmail.com>
2012-08-26 11:38:51 -0400

    auto check in

--- static/app/geomap.js
+++ static/app/geomap.js
@@ -115,7 +115,7 @@ $(document).ready(function(){
     google.maps.event.addListener(polygon, "mouseover", function(event){
       infowindow.setPosition(event.latLng);
       infowindow.open(GeoMap.map);
-      polygon.setOptions({strokeWeight:1, strokeOpacity: 1});
+      polygon.setOptions({strokeWeight:2});
     });
     google.maps.event.addListener(polygon, "mouseout", function(event){
       infowindow.close();
